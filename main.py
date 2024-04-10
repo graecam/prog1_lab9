9ced4cd681467311c4c7a3f8c19df6029b65f9b0
graecam <166649022+graecam@users.noreply.github.com>
2024-04-10 15:31:48 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+while True:
+    print("Menu\n--------------\n1. Encode\n2. Decode\n3. Quit")
+
+    option = int(input("Please enter an option: "))
+    if option == 3:
+        break
+
+
+
+    if option == 1:
+        password = input("Please enter your password to encode: ")
+        encoded_list = []
+        for digit in list(password):
+            new_digit = int(digit)+3
+            encoded_list.append(new_digit)
+        print(encoded_list)
+        print("Your password has been encoded and stored!")
+
+    elif option == 2:
+        pass
+
