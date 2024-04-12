06b308958cb87cdcf2505d602980398ef7efaa4a
graecam <166649022+graecam@users.noreply.github.com>
2024-04-12 18:21:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ while True:
 
     if option == 1:
         password = input("Please enter your password to encode: ")
-        encoded_list = []
+        encoded_list = ''
         for digit in list(password):
             new_digit = int(digit)+3
-            encoded_list.append(new_digit)
-        print(encoded_list)
+            encoded_list  += str(new_digit)
+       
         print("Your password has been encoded and stored!")
 
     elif option == 2:
