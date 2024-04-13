d01f769e8810b354e1e305bdd612c17bb6720b35
Jordan Hall <166601247+Pixi-J@users.noreply.github.com>
2024-04-12 21:45:14 -0400

    Updated main
    Altered encoded_list to type(list) to make the output look cleaner

--- main.py
+++ main.py
@@ -14,13 +14,24 @@ while True:
 
     if option == 1:
         password = input("Please enter your password to encode: ")
-        encoded_list = ''
+        encoded_list = []
         for digit in list(password):
             new_digit = int(digit)+3
-            encoded_list  += str(new_digit)
+            encoded_list.append(new_digit)
        
         print("Your password has been encoded and stored!")
 
     elif option == 2:
-        pass
+        decoded = []
+        for digit in encoded_list:
+            decoded_digit = int(digit) - 3
+            decoded.append(decoded_digit)
+            decoded_str = ''
+            encoded_str = ''
+            for item in decoded:
+                decoded_str += str(item)
+            for item in list(encoded_list):
+                encoded_str += str(item)
+        print("The encoded password is", encoded_str,", and the original password is", decoded_str,".")
+
 
